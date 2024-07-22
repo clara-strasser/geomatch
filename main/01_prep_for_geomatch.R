72b6ccea5e8b3dee337c568f5c484739dbe3cab9
Clara Strasser <clarastrasser@MacBook-Pro-von-Clara.fritz.box>
2024-07-22 11:51:52 +0200

    corrected formats of variables

--- main/01_prep_for_geomatch.R
+++ main/01_prep_for_geomatch.R
@@ -65,8 +65,19 @@ data <- data %>%
 ## 3) Correct formats ----------------------------------------------------------
 
 data <- data %>%
-  mutate(aid = as.character(aid))
-
+  mutate(aid = as.character(aid),
+         sex = as.factor(sex),
+         free_case = as.factor(free_case),
+         corigin = as.factor(corigin),
+         refugee_sample = as.factor(refugee_sample),
+         religious_affiliation = as.factor(religious_affiliation),
+         german_speaking = as.factor(german_speaking),
+         german_writing = as.factor(german_writing),
+         german_reading = as.factor(german_reading),
+         school_degree_low = as.factor(school_degree_low),
+         school_degree_med = as.factor(school_degree_med),
+         school_degree_high = as.factor(school_degree_high),
+         vocational_training = as.factor(vocational_training))
 
 # Save data --------------------------------------------------------------------
 
